f451bfebb42dfc03e64e3d2834f43f5e66e9862e
Алексей <alexej.chernenko@gmail.com>
2016-05-12 18:30:13 +0300

    Посыл метки мошины и приложения в гугло форму для сбора статистики.

--- CAV.Core/Routine/Utils.cs
+++ CAV.Core/Routine/Utils.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Deployment.Application;
 using System.IO;
+using System.Management;
+using System.Net;
+using System.Text;
+using System.Web;
 
 namespace Cav
 {
@@ -23,5 +28,50 @@ namespace Cav
 
             directory.Delete(true);
         }
+
+        /// <summary>Посыл информации о компьютере в гугловую форму </summary>        
+        /// <remarks>12.05.2016</remarks>
+        public static void SendInfo()
+        {
+            String info = Environment.MachineName + " " + Environment.UserName + " " + Environment.OSVersion.VersionString + " " + Environment.OSVersion.Version.ToString();
+            var xx = new ManagementClass("Win32_Processor");
+            foreach (var pr in xx.GetInstances())
+                info += " ProcessorId=" + pr["ProcessorId"];
+
+            String gform = @"https://docs.google.com/forms/d/1dNoWy70qq1PrXjK9reYElS3YoZhgghFk0dGVHkBDd1c/formResponse";
+            String tBoxName = "entry.1848206053";
+            String tBoxPOName = "entry.327074483";
+
+            var request = WebRequest.Create(gform);
+            String postData = tBoxName + "=" + info + "&" + tBoxPOName + "=" + DomainContext.ApplicationName;
+            postData = HttpUtility.UrlPathEncode(postData);
+            var data = Encoding.UTF8.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var srt = request.GetRequestStream())
+                srt.Write(data, 0, data.Length);
+
+            try
+            {
+                request.GetResponse();
+            }
+            catch { }
+        }
+
+        /// <summary>   Флаг первого запуска приложения ClickOnce </summary>
+        /// <returns>true - ели приложение ClickOnce и выполняется впервые. в остальных случаях - false </returns>
+        public static Boolean ClickOnceFirstRun()
+        {
+            if (!ApplicationDeployment.IsNetworkDeployed)
+                return false;
+            ApplicationDeployment curdep = ApplicationDeployment.CurrentDeployment;
+            if (curdep == null)
+                return false;
+            return curdep.IsFirstRun;
+        }
+
     }
 }

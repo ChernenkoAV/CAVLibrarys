6532c5fe77468c48adc6568ae812220e3e45d36b
Алексей <alexej.chernenko@gmail.com>
2017-12-19 19:14:12 +0300

    Добавил хелпер по созданию службы на базе смев. Поправил имена параметров.

--- CAV.Core/Soap/SoapHelper.cs
+++ CAV.Core/Soap/SoapHelper.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.ServiceModel.Dispatcher;
 using System.ServiceModel.Security;
 using System.Xml.Linq;
+using Cav.DigitalSignature;
 
 namespace Cav.Soap
 {
@@ -139,70 +141,73 @@ namespace Cav.Soap
         /// Создание клиента в обертке в котором сообщение подписывается(На базе биндинга для СМЭВ)
         /// </summary>
         /// <typeparam name="T">Тип клиента, наследник от <c>ClientBase&lt;TChannel&gt;</c></typeparam>
-        /// <param name="Uri">Uri сервиса</param>
-        /// <param name="ClientSert">Клиентский сертификат</param>
-        /// <param name="ServerSert">Серверный сертификат</param>
-        /// <param name="AlgorithmSuite">Алгоритм подписи</param>
-        /// <param name="LoggerType">Тип логгера</param>
-        /// <param name="LoggerInstanse">Экземпляр логгера(приоритетнее при указании и экземпляра и типа логгера)</param>
-        /// <param name="EnableUnsecuredResponse">Задает значение, указывающее, может ли отправлять и получать небезопасные ответы или безопасные запросы.</param>
-        /// <param name="SenderActor">Actor отправителя</param>
-        /// <param name="RecipientActor">Actor получателя</param>
-        /// <param name="Proxy">Прокси для клиента</param>
-        /// <param name="SendTimeout">Таймаут работы клиента</param>
-        /// <param name="AllowInsecureTransport">Можно ли отправлять сообщения в смешанном режиме безопасности</param>
+        /// <param name="uri">Uri сервиса</param>
+        /// <param name="clientSert">Клиентский сертификат</param>
+        /// <param name="serverSert">Серверный сертификат</param>
+        /// <param name="algorithmSuite">Если null, то берется <see cref="CryptoProRutine.CriptoProBasicGostObsolete"/></param>
+        /// <param name="loggerType">Тип логгера</param>
+        /// <param name="loggerInstanse">Экземпляр логгера(приоритетнее при указании и экземпляра и типа логгера)</param>
+        /// <param name="enableUnsecuredResponse">Задает значение, указывающее, может ли отправлять и получать небезопасные ответы или безопасные запросы.</param>
+        /// <param name="senderActor">Actor отправителя</param>
+        /// <param name="recipientActor">Actor получателя</param>
+        /// <param name="proxy">Прокси для клиента</param>
+        /// <param name="sendTimeout">Таймаут работы клиента</param>
+        /// <param name="allowInsecureTransport">Можно ли отправлять сообщения в смешанном режиме безопасности</param>
         /// <returns>Обертка с клиентом</returns>
         public static WrapClient<T> CreateSmevClient<T>(
-            String Uri,
-            SecurityAlgorithmSuite AlgorithmSuite,
-            X509Certificate2 ClientSert,
-            X509Certificate2 ServerSert = null,
-            Type LoggerType = null,
-            ISoapPackageLog LoggerInstanse = null,
-            Boolean EnableUnsecuredResponse = false,
-            String SenderActor = null,
-            String RecipientActor = null,
-            String Proxy = null,
-            TimeSpan? SendTimeout = null,
-            Boolean AllowInsecureTransport = false)
+            String uri,
+            X509Certificate2 clientSert,
+            X509Certificate2 serverSert = null,
+            SecurityAlgorithmSuite algorithmSuite = null,
+            Type loggerType = null,
+            ISoapPackageLog loggerInstanse = null,
+            Boolean enableUnsecuredResponse = false,
+            String senderActor = null,
+            String recipientActor = null,
+            String proxy = null,
+            TimeSpan? sendTimeout = null,
+            Boolean allowInsecureTransport = false)
             where T : class, ICommunicationObject, IDisposable
         {
 
-            if (ServerSert == null)
-                ServerSert = ClientSert;
+            if (serverSert == null)
+                serverSert = clientSert;
 
-            EndpointAddress ea = new EndpointAddress(new Uri(Uri), DnsEndpointIdentity.CreateDnsIdentity(ServerSert.GetNameInfo(X509NameType.SimpleName, false)));
+            EndpointAddress ea = new EndpointAddress(new Uri(uri), DnsEndpointIdentity.CreateDnsIdentity(serverSert.GetNameInfo(X509NameType.SimpleName, false)));
 
-            ISoapPackageLog logger = LoggerInstanse;
+            ISoapPackageLog logger = loggerInstanse;
 
             if (logger == null)
                 try
                 {
-                    logger = (ISoapPackageLog)Activator.CreateInstance(LoggerType);
+                    logger = (ISoapPackageLog)Activator.CreateInstance(loggerType);
                 }
                 catch { }
 
-            var binding = SmevBinding.Create(
-                algorithmSuite: AlgorithmSuite,
-                proxy: Proxy,
-                loggerInstance: logger,
-                senderActor: SenderActor,
-                recipientActor: RecipientActor,
-                enableUnsecuredResponse: EnableUnsecuredResponse,
-                allowInsecureTransport: AllowInsecureTransport);
+            if (algorithmSuite == null)
+                algorithmSuite = CryptoProRutine.CriptoProBasicGostObsolete;
 
-            if (SendTimeout.HasValue)
-                binding.SendTimeout = SendTimeout.Value;
+            var binding = SmevBinding.Create(
+                algorithmSuite: algorithmSuite,
+                proxy: proxy,
+                loggerInstance: logger,
+                senderActor: senderActor,
+                recipientActor: recipientActor,
+                enableUnsecuredResponse: enableUnsecuredResponse,
+                allowInsecureTransport: allowInsecureTransport);
+
+            if (sendTimeout.HasValue)
+                binding.SendTimeout = sendTimeout.Value;
 
             T client = (T)Activator.CreateInstance(typeof(T), binding, ea);
 
             ClientCredentials cc = (ClientCredentials)client.GetType().GetProperty("ClientCredentials").GetValue(client, null);
-            if (ClientSert == null)
+            if (clientSert == null)
                 throw new NullReferenceException("Не указан клиентский сертификат. Либо не найден в хранилище, либо строка с BASE64 невалидна.");
-            cc.ClientCertificate.Certificate = ClientSert;
-            if (ServerSert == null)
+            cc.ClientCertificate.Certificate = clientSert;
+            if (serverSert == null)
                 throw new NullReferenceException("Не указан серверный сертификат. Либо не найден в хранилище, либо строка с BASE64 невалидна.");
-            cc.ServiceCertificate.DefaultCertificate = ServerSert;
+            cc.ServiceCertificate.DefaultCertificate = serverSert;
             cc.ServiceCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.None;
             cc.ServiceCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
 
@@ -225,41 +230,41 @@ namespace Cav.Soap
         /// Если клиент реализует ISoapPackageLog, то его экземпляр используется для логгирования.
         /// </summary>
         /// <typeparam name="T">Тип клиента, наследник от <c>ClientBase&lt;TChannel&gt;</c></typeparam>
-        /// <param name="Uri">Uri сервиса</param>
-        /// <param name="Proxy">Прокси для клиента</param>
-        /// <param name="LoggerType">Тип логгера</param>
-        /// <param name="LoggerInstanse">Экземпляр логгера</param>
-        /// <param name="SendTimeout">Таймаут посыла сообщения</param>
+        /// <param name="uri">Uri сервиса</param>
+        /// <param name="proxy">Прокси для клиента</param>
+        /// <param name="loggerType">Тип логгера</param>
+        /// <param name="loggerInstanse">Экземпляр логгера</param>
+        /// <param name="sendTimeout">Таймаут посыла сообщения</param>
         /// <returns></returns>
         public static WrapClient<T> CreateBasicClient<T>(
-            String Uri,
-            String Proxy = null,
-            Type LoggerType = null,
-            ISoapPackageLog LoggerInstanse = null,
-            TimeSpan? SendTimeout = null)
+            String uri,
+            String proxy = null,
+            Type loggerType = null,
+            ISoapPackageLog loggerInstanse = null,
+            TimeSpan? sendTimeout = null)
             where T : class, ICommunicationObject, IDisposable
         {
-            var ea = new EndpointAddress(Uri);
+            var ea = new EndpointAddress(uri);
 
             var binding = new BasicHttpBinding() { MaxReceivedMessageSize = int.MaxValue };
-            if (!Proxy.IsNullOrWhiteSpace())
+            if (!proxy.IsNullOrWhiteSpace())
             {
-                binding.ProxyAddress = new Uri(Proxy);
+                binding.ProxyAddress = new Uri(proxy);
                 binding.UseDefaultWebProxy = false;
             }
 
-            if (SendTimeout.HasValue)
-                binding.SendTimeout = SendTimeout.Value;
+            if (sendTimeout.HasValue)
+                binding.SendTimeout = sendTimeout.Value;
 
             T client = (T)Activator.CreateInstance(typeof(T), binding, ea);
 
-            ISoapPackageLog logger = LoggerInstanse;
+            ISoapPackageLog logger = loggerInstanse;
             if (logger == null)
                 logger = client as ISoapPackageLog;
             if (logger == null)
                 try
                 {
-                    logger = (ISoapPackageLog)Activator.CreateInstance(LoggerType);
+                    logger = (ISoapPackageLog)Activator.CreateInstance(loggerType);
                 }
                 catch { } // ну, не судьба
 
@@ -292,6 +297,54 @@ namespace Cav.Soap
             }
 
             ((IDisposable)client).Dispose();
+        }
+
+        /// <summary>
+        /// Создание хоста web-службы СМЭВ
+        /// </summary>
+        /// <param name="implementType">Класс, реализующий контракт службы</param>
+        /// <param name="endpointUri">Конечная точка службы</param>
+        /// <param name="servSert">Серверный сертификат</param>
+        /// <param name="contractType">Тип, определяющий контракт службы </param>        
+        /// <param name="senderActor">Актор сервиса</param>
+        /// <param name="recipientActor">Актор клиента</param>
+        /// <param name="algorithmSuite">Если null, то берется <see cref="CryptoProRutine.CriptoProBasicGostObsolete"/></param>
+        /// <returns></returns>
+        public static ServiceHost CreateSmevHost(
+            Type implementType,
+            Uri endpointUri,
+            String servSert,
+            Type contractType = null,
+            String senderActor = "http://smev.gosuslugi.ru/actors/recipient",
+            String recipientActor = "http://smev.gosuslugi.ru/actors/smev",
+            SecurityAlgorithmSuite algorithmSuite = null)
+        {
+            var cert = DSGeneric.FindCertByThumbprint(servSert);
+            if (cert == null)
+                throw new InvalidOperationException("Не найден сертификат");
+
+            if (contractType == null)
+                contractType = implementType;
+
+            if (algorithmSuite == null)
+                algorithmSuite = CryptoProRutine.CriptoProBasicGostObsolete;
+
+            ServiceHost res = new ServiceHost(implementType);
+
+            var sep = res.AddServiceEndpoint(
+                implementedContract: contractType,
+                binding: SmevBinding.Create(
+                    algorithmSuite: algorithmSuite,
+                    senderActor: senderActor,
+                    recipientActor: recipientActor),
+                address: endpointUri);
+
+            res.Credentials.ServiceCertificate.Certificate = cert;
+            res.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.None;
+
+            sep.Contract.ProtectionLevel = ProtectionLevel.Sign;
+
+            return res;
         }
     }
 }

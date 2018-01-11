2ced3ef4e3e5393cbe51a8664aa2414d54b145ba
Алексей <alexej.chernenko@gmail.com>
2018-01-11 15:32:24 +0300

    Переработана логика формирования текста при развертывании исключений.

--- CAV.Core/Routine/Extentions/ExtException.cs
+++ CAV.Core/Routine/Extentions/ExtException.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.ServiceModel;
 
 namespace Cav
 {
@@ -9,20 +10,54 @@ namespace Cav
     public static class ExtException
     {
         /// <summary>
-        /// Развертывание текста исключения + обработка SqlException
+        /// Развертывание текста исключения
         /// </summary>
         /// <param name="ex"></param>
         /// <returns></returns>
         public static String Expand(this Exception ex)
         {
-            return
-                ex == null
-                ? String.Empty
-                : (ex is SqlException
-                    ? "SqlException №:" + ((SqlException)ex).Number + " " + ex.Message
-                    : "Message: " + ex.Message + (ex.StackTrace.IsNullOrWhiteSpace() ? String.Empty : Environment.NewLine + "StackTrace: " + ex.StackTrace)) +
-                  Environment.NewLine +
-                  ex.InnerException.Expand();
+            String res = null;
+            if (ex == null)
+                return res;
+
+            res = $"Message: {ex.Message}{Environment.NewLine}";
+
+            SqlException sqlex = ex as SqlException;
+            if (sqlex != null)
+                res = $"Sql Server Number: {sqlex.Number}{Environment.NewLine}";
+
+            res += $"Type: {ex.GetType().FullName}{Environment.NewLine}";
+
+            if (ex.TargetSite != null)
+                res += $"TargetSite: {ex.TargetSite.ToString()}{Environment.NewLine}";
+
+            if (!ex.StackTrace.IsNullOrWhiteSpace())
+                res += $"StackTrace->{ex.StackTrace}{Environment.NewLine}";
+
+            FaultException commEx = ex as FaultException;
+            if (commEx != null)
+            {
+                ExceptionDetail exdetail = commEx.GetPropertyValueNestedObject("Detail") as ExceptionDetail;
+
+                if (exdetail != null)
+                    res += Environment.NewLine.PadLeft(5, '*');
+
+                while (exdetail != null)
+                {
+                    res += $"Detail Type: {exdetail.Type}{Environment.NewLine}";
+                    res += $"Detail Message: {exdetail.Message}{Environment.NewLine}";
+                    if (!exdetail.StackTrace.IsNullOrWhiteSpace())
+                        res += $"Detail StackTrace->{Environment.NewLine}{exdetail.StackTrace}{Environment.NewLine}";
+                    if (exdetail.InnerException != null)
+                        res += $"***{Environment.NewLine}Detail InnerException->{Environment.NewLine}";
+                    exdetail = exdetail.InnerException;
+                }
+            }
+
+            if (ex.InnerException != null)
+                res += $"{Environment.NewLine.PadLeft(20, '-')}InnerException->{Environment.NewLine}{ex.InnerException.Expand()}";
+
+            return res;
         }
     }
 }

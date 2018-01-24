4ca227fd14ef2d927325b4a5bcc12c3c3c16b0c4
Алексей <alexej.chernenko@gmail.com>
2018-01-24 11:39:19 +0300

    Добавил усечение для даты времени

--- CAV.Core/Routine/Extentions/ExtDateTime.cs
+++ CAV.Core/Routine/Extentions/ExtDateTime.cs
@@ -40,5 +40,69 @@ namespace Cav
         }
 
         #endregion
+
+        /// <summary>
+        /// Усечение даты-времени (скопированно с https://stackoverflow.com/questions/1004698/how-to-truncate-milliseconds-off-of-a-net-datetime)
+        /// </summary>
+        /// <param name="self">Экземпляр <see cref="System.DateTime"/></param>
+        /// <param name="resolution">Точность, до которой усечь</param>
+        /// <returns></returns>
+        public static DateTime Truncate(this DateTime self, DateTimeTruncateResolution resolution = DateTimeTruncateResolution.Second)
+        {
+            switch (resolution)
+            {
+                case DateTimeTruncateResolution.Year:
+                    return new DateTime(self.Year, 1, 1, 0, 0, 0, 0, self.Kind);
+                case DateTimeTruncateResolution.Month:
+                    return new DateTime(self.Year, self.Month, 1, 0, 0, 0, self.Kind);
+                case DateTimeTruncateResolution.Day:
+                    return new DateTime(self.Year, self.Month, self.Day, 0, 0, 0, self.Kind);
+                case DateTimeTruncateResolution.Hour:
+                    return self.AddTicks(-(self.Ticks % TimeSpan.TicksPerHour));
+                case DateTimeTruncateResolution.Minute:
+                    return self.AddTicks(-(self.Ticks % TimeSpan.TicksPerMinute));
+                case DateTimeTruncateResolution.Second:
+                    return self.AddTicks(-(self.Ticks % TimeSpan.TicksPerSecond));
+                case DateTimeTruncateResolution.Millisecond:
+                    return self.AddTicks(-(self.Ticks % TimeSpan.TicksPerMillisecond));
+                default:
+                    throw new ArgumentException(nameof(resolution));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Точность, до которой усечь экземпляр <see cref="System.DateTime"/>
+    /// </summary>
+    public enum DateTimeTruncateResolution
+    {
+        /// <summary>
+        /// Год
+        /// </summary>
+        Year,
+        /// <summary>
+        /// Месяц
+        /// </summary>
+        Month,
+        /// <summary>
+        /// День
+        /// </summary>
+        Day,
+        /// <summary>
+        /// Час
+        /// </summary>
+        Hour,
+        /// <summary>
+        /// Минута
+        /// </summary>
+        Minute,
+        /// <summary>
+        /// Секунда
+        /// </summary>
+        Second,
+        /// <summary>
+        /// Миллисекунда
+        /// </summary>
+        Millisecond
     }
 }

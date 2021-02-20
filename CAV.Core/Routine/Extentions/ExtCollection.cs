e4cf4818b2fe3a86e860d76f40cd1c9dfe3300e3
ChernenkoAV <alexej.chernenko@gmail.com>
2021-02-21 01:12:50 +0300

    Добавил расшиирение для коллекции для действия над элементами с возвратом коллекции

--- CAV.Core/Routine/Extentions/ExtCollection.cs
+++ CAV.Core/Routine/Extentions/ExtCollection.cs
@@ -15,26 +15,26 @@ namespace Cav
         /// Соеденяет значения в коллекции с заданым разделителем
         /// </summary>
         /// <typeparam name="T">Тип идентификатора</typeparam>
-        /// <param name="values">Значения</param>
+        /// <param name="source">Значения</param>
         /// <param name="separator">Разделитель</param>
         /// <param name="distinct">Только уникальные значения</param>
         /// <param name="format">Формат преобразования к строке каждого объекта в коллекции(по умолчанию "{0}")</param>
         /// <returns>Значения разделенные разделителем</returns>
         public static string JoinValuesToString<T>(
-            this IEnumerable<T> values,
+            this IEnumerable<T> source,
             string separator = ",",
             Boolean distinct = true,
             String format = null)
         {
-            if (values == null)
+            if (source == null)
                 return null;
 
-            if (values.Count() == 0)
+            if (source.Count() == 0)
                 return null;
 
-            var vals = values;
+            var vals = source;
             if (distinct)
-                vals = values.Distinct();
+                vals = source.Distinct();
 
             if (!typeof(T).IsValueType)
                 vals = vals.Where(x => x != null).ToArray();
@@ -47,12 +47,34 @@ namespace Cav
         /// <summary>
         /// AddRange для коллекций, в которых этого расширения(метода) нет
         /// </summary>
-        /// <param name="cT">Collection</param>
-        /// <param name="collection">Коллекция для вставки</param>
-        public static void AddRange<T>(this Collection<T> cT, IEnumerable<T> collection)
+        /// <param name="target">Целевая коллекция</param>
+        /// <param name="source">Коллекция для вставки</param>
+        public static void AddRange<T>(this Collection<T> target, IEnumerable<T> source)
         {
-            foreach (var item in collection)
-                cT.Add(item);
+            foreach (var item in source)
+                target.Add(item);
+        }
+
+        /// <summary>
+        /// Выполнение действия над элементами коллекции с возвратом коллекции
+        /// </summary>
+        /// <typeparam name="T">Тип объектов коллекции</typeparam>
+        /// <param name="source">Исходная коллекция</param>
+        /// <param name="action">Действие над элементом</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Do<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            foreach (var item in source)
+            {
+                action(item);
+                yield return item;
+            }
         }
     }
 }

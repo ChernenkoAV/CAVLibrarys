6e418ef0860f4b4eec15290ad1f10e6ddecf76a6
ChernenkoAV <alexej.chernenko@gmail.com>
2022-12-10 02:49:12 +0300

    Добавлена архивация/разархивация zip-файлов средствами ZipArchive

--- src/Cav.Core/Routine/Extentions/ExtGZip.cs
+++ src/Cav.Core/Routine/Extentions/ExtGZip.cs
@@ -1,10 +1,12 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
+using System.Linq;
 
 namespace Cav
 {
     /// <summary>
-    /// Работа с GZip .Net
+    /// Работа с GZip и ZipArchive
     /// </summary>
     public static class ExtGZip
     {
@@ -49,6 +51,64 @@ namespace Cav
 
                 return result.ToArray();
             }
+        }
+
+        /// <summary>
+        /// Упаковать файлы в архив (в памяти). Работа с ZipArchive - все правила и ошибки его
+        /// </summary>
+        /// <param name="files">Коллекция "относительный путь в архиве"-"тело файла". Относительный путь - entryName в <see cref="ZipArchive.CreateEntry(string)"/></param>
+        /// <returns>Тело архива, Если <paramref name="files"/> null или пуст - возвращается null </returns>
+
+        public static byte[] ZipArhiveCompress(this Dictionary<string, byte[]> files)
+        {
+            if (files == null || !files.Any())
+                return null;
+
+            var memoryStream = new MemoryStream(); // для него юзинга нет, так как его закроет ZipArchive
+
+            using (var zipArchive = new ZipArchive(memoryStream, ZipArchiveMode.Create))
+            {
+                foreach (var f in files)
+                {
+                    var fileEntry = zipArchive.CreateEntry(f.Key);
+                    using (var writer = new BinaryWriter(fileEntry.Open()))
+                        writer.Write(f.Value);
+                }
+            }
+
+            return memoryStream.ToArray();
+        }
+
+        /// <summary>
+        /// Распаковать архив (в памяти). Работа с ZipArchive - все правила и ошибки его
+        /// </summary>
+        /// <param name="arc">Тело архива</param>
+        /// <returns>Коллекция "относительный путь в архиве"-"тело файла". Относительный путь - <see cref="ZipArchiveEntry.FullName"/> </returns>
+        public static IEnumerable<KeyValuePair<string, byte[]>> ZipArhiveDecompress(this byte[] arc)
+        {
+            if (arc == null || !arc.Any())
+                return null;
+
+            var res = new List<KeyValuePair<string, byte[]>>();
+
+            using (var memoryStream = new MemoryStream(arc))
+            using (var zipArchive = new ZipArchive(memoryStream, ZipArchiveMode.Read))
+            {
+                foreach (var zipArchiveEntry in zipArchive.Entries)
+                {
+                    byte[] body = null;
+                    using (var reader = new MemoryStream())
+                    using (var sStream = zipArchiveEntry.Open())
+                    {
+                        sStream.CopyTo(reader);
+                        body = reader.ToArray();
+                    }
+
+                    res.Add(new KeyValuePair<string, byte[]>(zipArchiveEntry.FullName, body));
+                }
+            }
+
+            return res;
         }
     }
 }

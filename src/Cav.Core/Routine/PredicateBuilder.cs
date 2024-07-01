a00a17b853304d98690a8f3c0af57d1d04f37117
ChernenkoAV <alexej.chernenko@gmail.com>
2024-07-01 09:49:27 +0300

    Форматирование пробелов

--- src/Cav.Core/Routine/PredicateBuilder.cs
+++ src/Cav.Core/Routine/PredicateBuilder.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq.Expressions;
+using System.Linq.Expressions;
 
 #pragma warning disable CA1062 // Проверить аргументы или открытые методы
 
@@ -9,43 +9,43 @@ namespace Cav;
 /// </summary>  
 public static class PredicateBuilder
 {
-    /// <summary>  
-    /// Creates a predicate that evaluates to true.  
-    /// </summary>  
+    /// <summary>
+    /// Creates a predicate that evaluates to true.
+    /// </summary>
     public static Expression<Func<T, bool>> True<T>() => param => true;
 
-    /// <summary>  
-    /// Creates a predicate that evaluates to false.  
-    /// </summary>  
+    /// <summary>
+    /// Creates a predicate that evaluates to false.
+    /// </summary>
     public static Expression<Func<T, bool>> False<T>() => param => false;
 
-    /// <summary>  
-    /// Creates a predicate expression from the specified lambda expression.  
-    /// </summary>  
+    /// <summary>
+    /// Creates a predicate expression from the specified lambda expression.
+    /// </summary>
     public static Expression<Func<T, bool>> Create<T>(Expression<Func<T, bool>> predicate) => predicate;
 
-    /// <summary>  
-    /// Combines the first predicate with the second using the logical "and".  
-    /// </summary>  
+    /// <summary>
+    /// Combines the first predicate with the second using the logical "and".
+    /// </summary>
     public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second) => first.compose(second, Expression.AndAlso);
 
-    /// <summary>  
-    /// Combines the first predicate with the second using the logical "or".  
-    /// </summary>  
+    /// <summary>
+    /// Combines the first predicate with the second using the logical "or".
+    /// </summary>
     public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second) => first.compose(second, Expression.OrElse);
 
-    /// <summary>  
-    /// Negates the predicate.  
-    /// </summary>  
+    /// <summary>
+    /// Negates the predicate.
+    /// </summary>
     public static Expression<Func<T, bool>> Not<T>(this Expression<Func<T, bool>> expression)
     {
         var negated = Expression.Not(expression.Body);
         return Expression.Lambda<Func<T, bool>>(negated, expression.Parameters);
     }
 
-    /// <summary>  
-    /// Combines the first expression with the second using the specified merge function.  
-    /// </summary>  
+    /// <summary>
+    /// Combines the first expression with the second using the specified merge function.
+    /// </summary>
     private static Expression<T> compose<T>(this Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge)
     {
         // zip parameters (map from parameters of second to parameters of first)  
